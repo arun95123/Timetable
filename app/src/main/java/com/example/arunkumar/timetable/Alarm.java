f4570d6374723113c50f7c8d4d43a05fd125c5c7
Arunkumar <ezio123arun@gmail.com>
2015-12-21 13:58:59 +0530

    check

--- app/src/main/java/com/example/arunkumar/timetable/Alarm.java
+++ app/src/main/java/com/example/arunkumar/timetable/Alarm.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
@@ -18,7 +19,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Arunkumar on 11/24/2015.
@@ -35,6 +42,61 @@ import java.io.IOException;
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.alarm_layout);
+
+        final String loginurl="http://52.10.251.227:3000/login";
+
+        final ConnectionDetector cd = new ConnectionDetector(Alarm.this);
+        if(cd.isConnectingToInternet()){
+
+
+            JSONObject jsonobject;
+            final JSONParser jParser1 = new JSONParser();
+            List<NameValuePair> params1 = new ArrayList<NameValuePair>();
+            params1.add(new BasicNameValuePair("password","arun"));
+            params1.add(new BasicNameValuePair("username","arun"));
+
+
+            jsonobject = jParser1.makeHttpRequest(loginurl, "POST", params1);
+
+            try{
+                if(jsonobject!=null){
+
+                    String result=jsonobject.getString("success");
+
+                    if(result.equals("true"))
+                    {
+                        Toast.makeText(Alarm.this, "Goyala op", Toast.LENGTH_LONG).show();
+                    }
+
+                    else{
+                        Toast.makeText(Alarm.this, "Incorrect Username or Password", Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+                else{
+                    Toast.makeText(Alarm.this, "No Response From Server", Toast.LENGTH_LONG).show();
+
+
+                }
+
+
+
+            }catch (Exception e){
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+
+
+
+
+
+
+        }else{
+
+            Toast.makeText(Alarm.this, "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+
 
 
 
